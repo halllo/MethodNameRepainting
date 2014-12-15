de2f02aa4e4e836bc07994c8b9ce0f8a03a799a5
Manuel Naujoks <manuel.naujoks@stp-online.de>
2014-12-15 18:21:41 +0100

    asm info aktualisiert

--- UnderscoresInNames/Properties/AssemblyInfo.cs
+++ UnderscoresInNames/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IntraTextAdornmentSample")]
+[assembly: AssemblyTitle("UnderscoresInNames")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("IntraTextAdornmentSample")]
-[assembly: AssemblyCopyright("Copyright © Microsoft")]
+[assembly: AssemblyCompany("Manuel Naujoks")]
+[assembly: AssemblyProduct("UnderscoresInNames")]
+[assembly: AssemblyCopyright("Copyright © Manuel Naujoks")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
