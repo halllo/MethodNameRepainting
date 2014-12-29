de09de96b22167887a4b64beb84777e7b05646ba
Manuel Naujoks <mnaujoks@gmail.com>
2014-12-29 18:58:03 +0100

    adornment zeigt den methodennamen vollständig

--- UnderscoresInNames/MethodNameAdornment.cs
+++ UnderscoresInNames/MethodNameAdornment.cs
@@ -4,28 +4,41 @@ using System.Windows.Media;
 
 namespace UnderscoresInNames
 {
-	internal sealed class MethodNameAdornment : TextBlock
+	internal sealed class MethodNameAdornment : Grid
 	{
 		public MethodNameAdornment()
 		{
-			Background = Brushes.Yellow;
-			VerticalAlignment = VerticalAlignment.Top;
-			FontFamily = new FontFamily("Consolas");
-			FontSize = 13;
-			Width = 60;
-			Height = 12;
-			Text = "platzhalter";
+			Initialize();
+
+			var platzhalter = "platzhalter";
+			Width = platzhalter.Length * 7.2;
+			((TextBlock)Children[0]).Text = platzhalter;
 		}
 
 		internal MethodNameAdornment(MethodNameTag tag)
-			: this()
 		{
+			Initialize();
+
 			Update(tag);
+		}
+
+		private void Initialize()
+		{
+			Background = Brushes.Yellow;
+			Height = 12;
+			Children.Add(new TextBlock
+			{
+				Margin = new Thickness(0, -3, 0, 0),
+				VerticalAlignment = VerticalAlignment.Top,
+				FontFamily = new FontFamily("Consolas"),
+				FontSize = 13,
+			});
 		}
 
 		internal void Update(MethodNameTag tag)
 		{
-			this.Text = tag.Name;
+			((TextBlock)Children[0]).Text = tag.Name;
+			Width = tag.Name.Length * 7.2;
 		}
 	}
 }
